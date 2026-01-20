59e753d4224c5af93ae4ff8d873cdf1c67166b73
Musthak <musthakcool55@gmail.com>
2026-01-20 11:22:34 +0530

    3rd program

--- Battleship_style_game/SinkDotComGame.java
+++ Battleship_style_game/SinkDotComGame.java
@@ -1,5 +1,40 @@
 package Battleship_style_game;
 
-public class SinkDotComGame {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class SinkDotComGame {
+    public static void main(String[] args) {
+
+        DotCom dot = new DotCom();
+        ArrayList<Integer> locations = new ArrayList<>();
+
+        locations.add(2);
+        locations.add(3);
+        locations.add(4);
+
+        dot.locationCells = locations;
+
+        Scanner scanner = new Scanner(System.in);
+        int numOfGuesses = 0;
+        boolean isAlive = true;
+
+        System.out.println("🎯 Sink a Dot Com Game Started!");
+
+        while (isAlive) {
+            System.out.print("Enter a number (0-6): ");
+            int guess = scanner.nextInt();
+            numOfGuesses++;
+
+            String result = dot.checkYourself(guess);
+            System.out.println(result);
+
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("💥 You sunk the Dot Com!");
+                System.out.println("Total guesses: " + numOfGuesses);
+            }
+        }
+        scanner.close();
+    }
 }
